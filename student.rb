a9a4b33ad055ab14ce7254d42f824af9e63704f5
Tresor <kalumunabisimwa5@gmail.com>
2022-06-21 08:45:17 +0200

    add directory

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './person'
 class student < person
   def initialize(name, age, parent_permission)
     super(age, name, parent_permission)
-    @class_room = classroom.new
+    @class_room = classroom
   end
 end
 

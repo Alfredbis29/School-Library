8e5e7d8f2f199385f9df0c193fe6e1d38f222f33
Tresor <kalumunabisimwa5@gmail.com>
2022-06-21 08:19:26 +0200

    create class person and it object

---
+++ personal.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @age = age
+    @name = name
+    @id = rand(1..100)
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services
+    if of_age? || parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def of_age?
+    @age > 18
+  end
+  end

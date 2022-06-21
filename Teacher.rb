f1cb9c6a37bb79f3743f97dd0472f860ccf5de39
Tresor <kalumunabisimwa5@gmail.com>
2022-06-21 09:02:21 +0200

    createclass teacher

---
+++ Teacher.rb
@@ -0,0 +1,12 @@
+require './person'
+
+def initialize(age, specialization,name = "unknown", parent_permission = true)
+super(age, name, parent_permission)
+@specilization = specialization
+end
+
+def can_use_services
+  true
+ end
+end
+

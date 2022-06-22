98a8f48cb8b7b8aada9cc56ca4f2d15f45056205
Tresor <kalumunabisimwa5@gmail.com>
2022-06-22 16:35:06 +0200

    base decorator fix some issue

--- decorator.rb
+++ decorator.rb
@@ -1,10 +1,10 @@
-require './nameable'
+require_relative './nameable'
 class Decorator < Nameable
-def initialize(nameable = Nameable.name)
-  @nameable = nameable
- end
+  def initialize(nameable = Nameable.name)
+    @nameable = nameable
+  end
 
- def correct_name
-@nameable.correct_name
- end
+  def correct_name
+    @nameable.correct_name
+  end
 end

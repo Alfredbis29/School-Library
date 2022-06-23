df51f227547b4a58af4d2c7a3db9e344cdd500a8
Tresor <kalumunabisimwa5@gmail.com>
2022-06-23 23:49:47 +0200

    Create a person (teacher or student, not a plain Person).

--- book.rb
+++ book.rb
@@ -1,13 +1,9 @@
 class Book
-  attr_accessor :title, :author, :rentals
+  attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
-    @rentals = []
-  end
-
-  def add_rental(book, date)
-    Rental.new(date, self, book)
   end
 end

178b537bd35a1fbfafc5c043e4d616f22bfe0c3f
Tresor <kalumunabisimwa5@gmail.com>
2022-06-23 09:06:07 +0200

    add book class

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_accessor :title, :author
+  attr_reader :rental
+ def initialize(title, author)
+   @title = title
+   @author = author
+   @label = []
+ end
+
+ def add_rental(rentals)
+   @rentals.push(rentals)
+   runtals.book = self.rental
+ end
+end
+

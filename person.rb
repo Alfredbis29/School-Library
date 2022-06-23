17649196280b85c50f3c90e021fb4aa383061860
Tresor <kalumunabisimwa5@gmail.com>
2022-06-23 23:47:32 +0200

    Create a person (teacher or student, not a plain Person).

--- person.rb
+++ person.rb
@@ -1,39 +1,33 @@
 require './nameable'
-require './book'
 require './rental'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :rentals
 
-  def initialize(age, name = 'unknown', parent_permision: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
-    @parent_permision = parent_permision
-    @rentals = []
+    @name = name
+    @parent_permission = parent_permission
   end
 
-  def correct_name
-    @name
-  end
-
-  def can_use_services?
-    if of_age? || parent_permision
+  def can_use_services
+    if of_age? || parent_permission
       true
     else
       false
     end
   end
 
-  def add_rental(book, date)
-    Rental.new(date, self, book)
+  def correct_name
+    @name
   end
 
   private
 
   def of_age?
-    @age >= 18
+    @age > 18
   end
 end

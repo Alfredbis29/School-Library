06a588ea65d57f907f6fb1918a68def741a591cf
Tresor <kalumunabisimwa5@gmail.com>
2022-06-23 12:50:25 +0200

    many to many relationship

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,4 @@
-require_relative'./person'
-
+require_relative './person'
 class Teacher < Person
   def initialize(age, specialization, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)

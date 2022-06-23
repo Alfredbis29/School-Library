9538d13b3bed74c3ab72f76e27722480ed44c673
Tresor <kalumunabisimwa5@gmail.com>
2022-06-23 12:40:42 +0200

    modify personal.rb

--- rental.rb
+++ rental.rb
@@ -2,11 +2,10 @@ class Rental
   attr_accessor :date
   attr_reader :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, person, book)
     @date = date
     @person = person
     person.rentals << self
-
     @book = book
     book.rentals << self
   end

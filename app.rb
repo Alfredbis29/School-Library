70c3f511792c764ce93f1b5ecc0951e2af63b095
Tresor <kalumunabisimwa5@gmail.com>
2022-06-24 12:30:08 +0200

    rename branch

--- app.rb
+++ app.rb
@@ -154,7 +154,7 @@ class App
     puts '1. list all books'
     puts '2. list all people'
     puts '3. create a person (teacher or student)'
-    puts '4. create a book'
+    puts '4. create a bookW'
     puts '5. create a rental'
     puts '6. list all rentals for a given person id'
     puts '7. Exit'
